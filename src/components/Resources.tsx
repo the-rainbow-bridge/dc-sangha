256d4ca5f06bb211e8b1f32be4892e35a206ea42
Eleni Papanicolas <eleni.papanicolas@proton.me>
2024-03-28 14:59:57 -0400

    filter resources by type

--- src/components/Resources.tsx
+++ src/components/Resources.tsx
@@ -5,45 +5,123 @@ import { cn } from "@lib/utils"
 
 type Props = {
   tags: string[]
+  types: string[]
   data: CollectionEntry<"resources">[]
-}
+};
 
-export default function Blog({ data, tags }: Props) {
+export default function Blog({ data, tags, types }: Props) {
   const [filter, setFilter] = createSignal(new Set<string>())
-  const [resources, setResources] = createSignal<CollectionEntry<"resources">[]>([])
+  const [resources, setResources] = createSignal<
+    CollectionEntry<"resources">[]
+  >([])
 
   createEffect(() => {
-    setResources(data.filter((entry) => 
-      Array.from(filter()).every((value) => 
-        entry.data.tags.some((tag:string) => 
-          tag.toLowerCase() === String(value).toLowerCase()
-        )
-      )
-    ))
-  })
-
-  function toggleTag(tag: string) {
-    setFilter((prev) => 
-      new Set(prev.has(tag) 
-        ? [...prev].filter((t) => t !== tag) 
-        : [...prev, tag]
+    setResources(
+      data.filter((entry) =>
+        Array.from(filter()).every((value) => {
+          const bool =
+            entry.data.tags.some(
+              (tag: string) => tag.toLowerCase() === String(value).toLowerCase()
+            ) || entry.data.type === value
+          console.log(bool)
+          return bool
+        })
       )
     )
+  })
+
+  function toggle(tagOrType: string) {
+    setFilter((prev) => {
+      const set = new Set(
+        prev.has(tagOrType)
+          ? [...prev].filter((t) => t !== tagOrType)
+          : [...prev, tagOrType]
+      )
+      console.log(set)
+      return set
+    })
   }
 
   return (
     <div class="grid grid-cols-1 sm:grid-cols-3 gap-6">
       <div class="col-span-3 sm:col-span-1">
         <div class="sticky top-24">
-          <div class="text-sm font-semibold uppercase mb-2 text-black dark:text-white">Filter</div>
+          <div class="text-sm font-semibold uppercase mb-2 text-black dark:text-white">
+            Filter by type
+          </div>
+          <ul class="flex flex-wrap sm:flex-col gap-1.5">
+            <For each={types}>
+              {(type) => (
+                <li>
+                  <button
+                    onClick={() => toggle(type)}
+                    class={cn(
+                      "w-full px-2 py-1 rounded",
+                      "whitespace-nowrap overflow-hidden overflow-ellipsis",
+                      "flex gap-2 items-center",
+                      "bg-black/5 dark:bg-white/10",
+                      "hover:bg-black/10 hover:dark:bg-white/15",
+                      "transition-colors duration-300 ease-in-out",
+                      filter().has(type) && "text-black dark:text-white"
+                    )}
+                  >
+                    <svg
+                      class={cn(
+                        "size-5 fill-black/50 dark:fill-white/50",
+                        "transition-colors duration-300 ease-in-out",
+                        filter().has(type) && "fill-black dark:fill-white"
+                      )}
+                    >
+                      <use
+                        href={`/ui.svg#square`}
+                        class={cn(!filter().has(type) ? "block" : "hidden")}
+                      />
+                      <use
+                        href={`/ui.svg#square-check`}
+                        class={cn(filter().has(type) ? "block" : "hidden")}
+                      />
+                    </svg>
+                    {type}
+                  </button>
+                </li>
+              )}
+            </For>
+          </ul>
+
+          <div class="text-sm font-semibold uppercase mb-2 text-black dark:text-white">
+            Filter by tag
+          </div>
           <ul class="flex flex-wrap sm:flex-col gap-1.5">
             <For each={tags}>
               {(tag) => (
                 <li>
-                  <button onClick={() => toggleTag(tag)} class={cn("w-full px-2 py-1 rounded", "whitespace-nowrap overflow-hidden overflow-ellipsis", "flex gap-2 items-center", "bg-black/5 dark:bg-white/10", "hover:bg-black/10 hover:dark:bg-white/15", "transition-colors duration-300 ease-in-out", filter().has(tag) && "text-black dark:text-white")}>
-                    <svg class={cn("size-5 fill-black/50 dark:fill-white/50", "transition-colors duration-300 ease-in-out", filter().has(tag) && "fill-black dark:fill-white")}>
-                      <use href={`/ui.svg#square`} class={cn(!filter().has(tag) ? "block" : "hidden")} />
-                      <use href={`/ui.svg#square-check`} class={cn(filter().has(tag) ? "block" : "hidden")} />
+                  <button
+                    onClick={() => toggle(tag)}
+                    class={cn(
+                      "w-full px-2 py-1 rounded",
+                      "whitespace-nowrap overflow-hidden overflow-ellipsis",
+                      "flex gap-2 items-center",
+                      "bg-black/5 dark:bg-white/10",
+                      "hover:bg-black/10 hover:dark:bg-white/15",
+                      "transition-colors duration-300 ease-in-out",
+                      filter().has(tag) && "text-black dark:text-white"
+                    )}
+                  >
+                    <svg
+                      class={cn(
+                        "size-5 fill-black/50 dark:fill-white/50",
+                        "transition-colors duration-300 ease-in-out",
+                        filter().has(tag) && "fill-black dark:fill-white"
+                      )}
+                    >
+                      <use
+                        href={`/ui.svg#square`}
+                        class={cn(!filter().has(tag) ? "block" : "hidden")}
+                      />
+                      <use
+                        href={`/ui.svg#square-check`}
+                        class={cn(filter().has(tag) ? "block" : "hidden")}
+                      />
                     </svg>
                     {tag}
                   </button>
@@ -53,6 +131,7 @@ export default function Blog({ data, tags }: Props) {
           </ul>
         </div>
       </div>
+
       <div class="col-span-3 sm:col-span-2">
         <div class="flex flex-col">
           <div class="text-sm uppercase mb-2">
